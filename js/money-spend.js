26b169e07405fbd1bb2662b01242549105aad631
TanvirHasanAshik <tanvirashik41@gmail.com>
2022-10-19 10:52:37 +0600

    Done moy project

--- js/money-spend.js
+++ js/money-spend.js
@@ -11,6 +11,19 @@ document.getElementById('calculate').addEventListener('click', function () {
     const totalExpenses = document.getElementById('total-expenses');
     const balance = document.getElementById('balance');
 
+    if (totalIncome < 0 || isNaN(totalIncome)) {
+        checkNegative(totalIncome, 'err-income');
+    }
+    if (foodCost < 0 || isNaN(foodCost)) {
+        checkNegative(foodCost, 'err-food-cost');
+    }
+    if (rentCost < 0 || isNaN(rentCost)) {
+        checkNegative(rentCost, 'err-rent-cost');
+    }
+    if (clothesCost < 0 || isNaN(clothesCost)) {
+        checkNegative(clothesCost, 'err-clothes-cost');
+    }
+
     if (totalIncome && foodCost && rentCost && clothesCost != NaN) {
         let expenses = foodCost + rentCost + clothesCost;
         if (totalIncome >= expenses) {
@@ -30,12 +43,27 @@ document.getElementById('save-amount').addEventListener('click', function (e) {
     const balance = parseFloat(balanceInput.innerText);
     const depositSavingAmount = document.getElementById('deposit-amount');
     const remainingBalance = document.getElementById('remaining-balance');
+    const errMessage = document.getElementById('err-save-amount');
 
     if (balance != 0 && saveAmount != NaN) {
         let totalSaveAmount = balance * saveAmount / 100;
         depositSavingAmount.innerText = totalSaveAmount;
         remainingBalance.innerText = balance - totalSaveAmount;
     }
+
+    checkNegative(saveAmount, 'err-save-amount');
+
     saveAmountInput.value = '';
 })
 /* End Saving Amount and Remaining Balance */
+
+function checkNegative(data, inputId) {
+    const errMessage = document.getElementById(inputId);
+    try {
+        if (data < 0) throw "Cannot give negative value";
+        if (isNaN(data)) throw "Cannot give string value";
+
+    } catch (err) {
+        errMessage.innerText = err;
+    }
+}
